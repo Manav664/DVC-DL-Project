dd397cf283bdacc5ca259c13db86270842f723c7
manav <manavstudy663@gmail.com>
2022-07-08 12:24:35 +0530

    stage_01 added

---
+++ src/stage_01_load_split_save_data.py
@@ -0,0 +1,57 @@
+from distutils.command.config import config
+import imp
+import numpy as np
+import tensorflow as tf
+import logging
+import gc
+import argparse
+
+from src.utils.all_utils import *
+
+def load_split_save(config_path: str):
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+    logging.info("Data loaded and splitted successfully")
+
+    content = read_yaml(config_path=config_path)
+
+    data_dir_name = content["data"]["data_dir_name"]
+    data_dir_path = os.path.join(os.getcwd(),data_dir_name)
+    create_directory([data_dir_name])
+
+    train_images_file_name = content["data"]["train_images_file_name"]
+    train_images_file_path = os.path.join(data_dir_path,train_images_file_name)
+    test_images_file_name = content["data"]["test_images_file_name"]
+    test_images_file_path = os.path.join(data_dir_path,test_images_file_name)
+    train_labels_file_name = content["data"]["train_labels_file_name"]
+    train_labels_file_path = os.path.join(data_dir_path,train_labels_file_name)
+    test_labels_file_name = content["data"]["test_labels_file_name"]
+    test_labels_file_path = os.path.join(data_dir_path,test_labels_file_name)
+    
+    np.save(train_images_file_path,x_train)
+    np.save(test_images_file_path,x_test)
+    np.save(train_labels_file_path,y_train)
+    np.save(test_labels_file_path,y_test)
+    logging.info("Data saved sucessfully")
+
+    del x_train,y_train,x_test,y_test
+    gc.collect()
+
+if __name__ == "__main__":
+
+    args = argparse.ArgumentParser()
+    
+    default_config_path = os.path.join(os.path.join(os.getcwd(),"config"),"config.yaml")
+
+    args.add_argument("--config","-c",default=default_config_path)
+
+    parsed_args = args.parse_args()
+
+    initialize_log(config_path=parsed_args.config)
+
+    logging.info("\n <<<<<< stage 1 started")
+    load_split_save(config_path=parsed_args.config)
+    logging.info("\n stage 1 completed >>>>>>")
+
+
+
+
---
+++ src/utils/all_utils.py
@@ -0,0 +1,31 @@
+
+import yaml
+import os
+import logging
+
+def read_yaml(config_path: str) -> dict:
+    with open(config_path) as yaml_file:
+        content = yaml.safe_load(yaml_file)
+    return content
+
+def initialize_log(config_path: str):
+    logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
+
+    content = read_yaml(config_path=config_path)
+    log_dir_name = content["logs"]["log_dir_name"]
+    log_dir_path = os.path.join(os.getcwd(),log_dir_name)
+    log_file_name = content["logs"]["log_file_name"]
+    log_file_path = os.path.join(log_dir_path,log_file_name)
+    create_directory([log_dir_path])
+
+    logging.basicConfig(filename=log_file_path,
+                        filemode="w",
+                        format=logging_str,
+                        level=logging.INFO)
+    
+
+def create_directory(dir_paths: list):
+
+    for dir_path in dir_paths:
+        if not os.path.isdir(dir_path):
+            os.mkdir(dir_path)
